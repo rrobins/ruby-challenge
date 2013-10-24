dfa6e8b2c35bd8a6af45e324ce5489ab272327b8
Riley Robins <rileyrobins@live.ca>
2013-10-24 14:38:25 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RubyChallenge::Application.config.secret_token = '27881fdc3c8121f0e1e2957c8bc2838575063d7d993952dc236a73db1c940adb3519e1e05ec3332aac59dc82a0cca6355fc0599a9f49fbd68c6835c5f8e8484d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RubyChallenge::Application.config.session_store :cookie_store, key: '_ruby-challenge_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RubyChallenge::Application.config.session_store :active_record_store
